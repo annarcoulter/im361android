a686a594d5a808ed4ecdfe401b0ca2c41cd6406d
Anna Coulter <arcoulter2@mail.bradley.edu>
2019-03-25 18:47:23 -0500

    womp

--- app/src/main/java/com/anna/cafemeow/MainActivity.kt
+++ app/src/main/java/com/anna/cafemeow/MainActivity.kt
@@ -2,10 +2,18 @@ package com.anna.cafemeow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onMapReady(map: GoogleMap?) {
+        Log.i("CafeMeow", "Map is ready")
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     var counter = 0
 
@@ -18,5 +26,8 @@ class MainActivity : AppCompatActivity() {
             counter++
             myText.text = resources.getQuantityString(R.plurals.has_been_clicked, counter, counter)
         }
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.my_map_view) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 }
